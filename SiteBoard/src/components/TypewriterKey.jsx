b39deda24ee6af847b9e73fe7c9899937f9373c8
Andrew DGalga <ex.spake@gmail.com>
2023-10-10 20:50:00 -0500

    button audio effect.

--- SiteBoard/src/components/TypewriterKey.jsx
+++ SiteBoard/src/components/TypewriterKey.jsx
@@ -1,6 +1,15 @@
 
 export default function TypewriterKey({majorKey, minorKey}) {
+    const audioEffect = new Audio("/audio/TypewriterClickA.wav")
+
+    function playAudio(){
+        if(audioEffect.readyState === 4){
+            audioEffect.currentTime = 0;
+            audioEffect.play();
+        }
+    }
+
     return (
-        <button className="typewriterkey">{majorKey}</button>
+        <button className="typewriterkey" onClick={playAudio}>{majorKey}</button>
     );
 }
