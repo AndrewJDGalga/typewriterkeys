a850d620ab9684a2323de3f1c78ef51deefbf9af
Andrew DGalga <ex.spake@gmail.com>
2023-10-10 20:53:57 -0500

    page element

--- SiteBoard/src/App.jsx
+++ SiteBoard/src/App.jsx
@@ -1,9 +1,11 @@
 import TypewriterKey from "./components/TypewriterKey";
+import Page from "./components/Page";
 import './css_prod/mainstyle.css';
 
 export default function App() {
   return (
     <div className="typewriterapp"> 
+      <Page/>
       <TypewriterKey majorKey={"a"} minorKey={"A"} />
     </div>
   )
